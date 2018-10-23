9afd084db8c4cdc9befd6824db5ac5d7ac63ab52
Philip Rogers (Rogue) <33717739+PhilipRogers3@users.noreply.github.com>
2018-10-23 17:39:39 +0100

    week 1

---
+++ Assets/Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController2D : MonoBehaviour {
+    public float speed = 5.0f;
+    Rigidbody2D RB2D;
+	
+    // Use this for initialization
+	void Start ()
+    {
+        RB2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        RB2D.velocity = new Vector2(x, y) * speed;
+        RB2D.angularVelocity = 0.0f;
+	}
+}
