f7ee3e629797f37f21e31fdc08e5bdf5a54a1154
Philip Rogers (Rogue) <33717739+PhilipRogers3@users.noreply.github.com>
2018-11-20 12:50:07 +0000

    Adding Assets
    Adding the space assets to SpaceReign

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void BacktoMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
