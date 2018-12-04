471884dc496764bcb1ac05ed0d74c984eb4af8ab
Philip Rogers (Rogue) <33717739+PhilipRogers3@users.noreply.github.com>
2018-12-04 17:40:28 +0000

    week 11
    music and starting working on the shop

---
+++ Assets/Scripts/WeaponButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEbgine.UI;
+
+public class WeaponButton : MonoBehaviour
+{
+    public PlayerShooting playerShooting;
+    public int weaponNumber;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        SetButton();
+    }
+
+    void SetButton()
+    {
+        string costString = playerShooting.weapons[weaponNumber].cost.ToString();
+        name.Text = playerShooting.weapons[weaponNumber].name;
+        cost.text = "£" + playerShooting.weapons[weaponNumber].cost;
+        description.text = playerShooting.weapons[weaponNumber].description;
+    }
+
+    public void OnClick()
+    {
+        if (ScoreManager.score >= playerShooting.weapons [weaponNumber].cost)
+        {
+            ScoreManager.score -= playerShooting.weapons[weaponNumber].cost;
+            playerShooting.currentWeapon = weaponNumber;
+        } else
+        {
+            source.Play();
+        }
+    }
+	
+}
---
+++ Assets/Scripts/WeaponObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponObject : MonoBehaviour
+{
+    public string weaponName = "";
+    public int cost = 100;
+    public string description;
+
+    public float fireRate = .5f;
+    public int damage = 10;
+    public float range = 100;
+
+}
