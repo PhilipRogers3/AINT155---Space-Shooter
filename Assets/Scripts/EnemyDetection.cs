e39eae012037e5e679c2d62c71b635cff859d53c
Philip Rogers (Rogue) <33717739+PhilipRogers3@users.noreply.github.com>
2019-01-16 17:22:59 +0000

    nearly done(deadline tomorrow)
    come in and email james about the boss alien aim and the full screen problem

--- Assets/Scripts/EnemyDetection.cs
+++ Assets/Scripts/EnemyDetection.cs
@@ -5,18 +5,7 @@ using UnityEngine.Events;
 public class EnemyDetection : MonoBehaviour
 {
     public UnityEvent onDetectPlayer;
-    
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,4 +20,9 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene("Main Menu");
     }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
 }
